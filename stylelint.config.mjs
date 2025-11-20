2f57cd770e439d19ceb95fd501c4801059b9d26b
Roman Degtiar <roman@degtyar.cc>
2025-11-20 13:01:15 +0400

    adding stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+const config = {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-prettier/recommended",
+    "stylelint-config-concentric-order",
+  ],
+};
+
+export default config;
